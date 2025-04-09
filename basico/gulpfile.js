a9df1b8a6ed5f1e28397c50e9d40dcfb91767a63
ViniciusGomes <viniciuslucasvlcg@gmail.com>
2025-04-09 20:29:37 -0300

    Execução em série e de forma paralela

--- basico/gulpfile.js
+++ basico/gulpfile.js
@@ -1,9 +1,28 @@
 const gulp = require('gulp')
-const series = gulp.series
+const { series, parallel } = require('gulp')
+
+const antes1 = cb => {
+  console.log('Tarefa Antes 1!')
+  return cb()
+}
+
+const antes2 = cb => {
+  console.log('Tarefa Antes 2!')
+  return cb()
+}
 
 function copiar(cb) {
   console.log('Tarefa de copiar!')
   return cb()
 }
 
-exports.default = series(copiar)
+const fim = cb => {
+  console.log('Fim!')
+  return cb()
+}
+
+exports.default = series(
+  parallel(antes1, antes2),
+  copiar,
+  fim,
+)
